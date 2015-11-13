20b8b9495ca9499c35075088fc3828e9a6c69c99
William Warren <wbwarren@gmail.com>
2015-11-13 16:21:06 +0000

    program12.js

---
+++ program13.js
@@ -0,0 +1,35 @@
+// myFirst HTTP Server.js 
+
+//Lets require the needed modules
+var http = require('http');
+var url = require('url');
+
+
+// Define server, handler, and listen
+var server = http.createServer(function(req, res) {
+    var totalBytes = 0;
+    
+    res.on('finish', function() {
+        console.log('request all done; processed ' + totalBytes + ' total bytes');
+        totalBytes = 0;
+    });
+
+    // if not a POST, close connection
+    if (req.method != 'POST') {
+        console.log('request not POST; request = ' + req.method);
+        res.end();
+    }
+    
+    console.log('client request received = "' + req.method + ':  ' + req.url + '"');
+    req.pipe(t2m(function(chunk) {
+        var doodah = chunk.toString().toUpperCase();
+        totalBytes += doodah.length;
+        return doodah;
+    })).pipe(res);
+    
+    console.log('done chunks');
+});
+
+server.listen(Number(process.argv[2]), function() {
+    console.log('server is listening');
+});
