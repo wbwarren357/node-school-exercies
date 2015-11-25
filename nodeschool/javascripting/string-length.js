e40a5c97764cddc64579b4747c30bfade0a8e21b
William Warren <wbwarren@gmail.com>
2015-11-25 22:39:37 +0000

    string-length.js javascripting

---
+++ nodeschool/javascripting/string-length.js
@@ -0,0 +1,14 @@
+"use strict";
+
+
+// ## The challenge:  
+   
+//   Create a file named string-length.js.  
+   
+//   In that file, create a variable named example.  
+   
+//   Assign the string 'example string' to the variable example.  
+   
+//   Use console.log to print the length of the string to the terminal.  
+var example = 'example string';
+console.log(example.length);
