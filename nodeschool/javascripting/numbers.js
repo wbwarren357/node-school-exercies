7972ef3baa8f26a3724f16cff5471e02e8d54731
William Warren <wbwarren@gmail.com>
2015-11-25 22:49:35 +0000

    numbers.js javascripting

---
+++ nodeschool/javascripting/numbers.js
@@ -0,0 +1,14 @@
+"use strict";
+
+
+//  ## The challenge:  
+   
+//   Create a file named numbers.js.  
+   
+//   In that file define a variable named example that references the integer  
+//   123456789.  
+   
+//   Use console.log() to print that number to the terminal.  
+
+var example = 123456789;
+console.log(example);
