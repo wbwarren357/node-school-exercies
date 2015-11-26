f83123ca3525e3d0083cba491a03e366b762f189
William Warren <wbwarren@gmail.com>
2015-11-26 23:14:59 +0000

    functions.js

---
+++ nodeschool/javascripting/functions.js
@@ -0,0 +1,21 @@
+"use strict";
+
+
+//  ## The challenge:  
+   
+//   Create a file named functions.js.  
+   
+//   In that file, define a function named eat that takes an argument named  
+//   food that is expected to be a string.  
+   
+//   Inside the function return the food argument like this:  
+   
+//      return food + ' tasted really good.';  
+   
+//   Inside of the parentheses of console.log(), call the eat() function with  
+//   the string bananas as the argument.  
+
+function eat(food) {
+    return food + ' tasted really good.';
+}
+console.log(eat('bananas'));
