c47aae952b7c1fd0c539c00681fc227d3802ab9a
William Warren <wbwarren@gmail.com>
2015-11-26 23:05:41 +0000

    objects.js javascripting

---
+++ nodeschool/javascripting/objects.js
@@ -0,0 +1,23 @@
+"use strict";
+
+
+// ## The challenge:  
+
+//   Create a file named objects.js.  
+
+//   In that file, define a variable named pizza like this:  
+
+//      var pizza = {  
+//       toppings: ['cheese', 'sauce', 'pepperoni'],  
+//       crust: 'deep dish',  
+//       serves: 2  
+//      };  
+
+//   Use console.log() to print the pizza object to the terminal.  
+
+var pizza = {
+    toppings: ['cheese', 'sauce', 'pepperoni'],
+    crust: 'deep dish',
+    serves: 2
+};
+console.log(pizza);
