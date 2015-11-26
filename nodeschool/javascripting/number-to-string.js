d52658ff6817bdbb51ed0e24f78f150b65d5a948
William Warren <wbwarren@gmail.com>
2015-11-26 22:13:17 +0000

    number-to-string.js javascripting

---
+++ nodeschool/javascripting/number-to-string.js
@@ -0,0 +1,16 @@
+"use strict";
+
+
+//  ## The challenge:  
+   
+//   Create a file named number-to-string.js.  
+   
+//   In that file define a variable named n that references the number 128;  
+   
+//   Call the .toString() method on the n variable.  
+   
+//   Use console.log() to print the results of the .toString() method to the  
+//   terminal.  
+
+var n = 128;
+console.log(n.toString());
