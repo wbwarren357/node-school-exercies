1118e3a4fc57f30159f3bd9c6b871bf9cdf4e680
William Warren <wbwarren@gmail.com>
2015-11-26 23:09:38 +0000

    object-properties.js javascripting

---
+++ nodeschool/javascripting/object-properties.js
@@ -0,0 +1,20 @@
+"use strict";
+
+
+//  ## The challenge:  
+   
+//   Create a file named object-properties.js.  
+   
+//   In that file, define a variable named food like this:  
+   
+//      var food = {  
+//       types: 'only pizza'  
+//      };  
+   
+//   Use console.log() to print the types property of the food object to the  
+//   terminal.  
+
+var food = {
+    types: 'only pizza'
+};
+console.log(food.types);
