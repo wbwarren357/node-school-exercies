5faf14f473465b23ce90c186a11f3da5b9f78d3c
William Warren <wbwarren@gmail.com>
2015-11-26 22:55:11 +0000

    accessing-array-values.js javascripting

---
+++ nodeschool/javascripting/accessing-array-values.js
@@ -0,0 +1,15 @@
+"use strict";
+
+
+// ## The challenge:  
+   
+//   Create a file named accessing-array-values.js.  
+   
+//   In that file, define array food :  
+   
+//      var food = ['apple', 'pizza', 'pear'];  
+   
+//   Use console.log() to print the second value of array to the terminal.  
+   
+var food = ['apple', 'pizza', 'pear'];
+console.log(food[1]);
